de839c59606d7cf849e63d217a94201258cfc1a4
MangoFizz <ccs81.mr@gmail.com>
2023-07-15 15:33:41 -0600

    Notice when tag was not found

--- src/balltze/features/tag_data_importing/tag_data_importing.cpp
+++ src/balltze/features/tag_data_importing/tag_data_importing.cpp
@@ -277,11 +277,16 @@ namespace Balltze::Features {
                 else {
                     // Load tags
                     for(auto &tag : map.tags_to_load) {
+                        bool tag_found = false;
                         for(std::size_t i = 0; i < map_tag_data_header.tag_count; i++) {
                             std::string path = translate_address(tag_array_raw[i].path);
                             if(path == tag.first && tag_array_raw[i].primary_class == tag.second) {
+                                tag_found = true;
                                 load_tag(tag_array_raw + i, true);
                             }
+                        }
+                        if(!tag_found) {
+                            logger.debug("Tag {} of class {} not found in map {}", tag.first, tag.second, map.name);
                         }
                     }
                 }
