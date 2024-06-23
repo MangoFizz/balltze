0f1682a24348df4173d45e8ae034c28b69ef3b30
MangoFizz <ccs81.mr@gmail.com>
2024-06-23 06:00:32 -0600

    Fix Lua bitfield types methods

--- src/balltze/plugins/lua/helpers/bitfield.hpp
+++ src/balltze/plugins/lua/helpers/bitfield.hpp
@@ -7,14 +7,14 @@
     [](lua_State *state) -> int { \
         auto *self = luacs_to_object(state, 1, bitfieldType); \
         int args = lua_gettop(state); \
-        if(args == 1) { \
-            if(lua_isboolean(state, 1)) { \
-                self->field = lua_toboolean(state, 1) ? 1 : 0; \
+        if(args == 2) { \
+            if(lua_isboolean(state, 2)) { \
+                self->field = lua_toboolean(state, 2) ? 1 : 0; \
             } else { \
                 return luaL_error(state, "Expected boolean"); \
             } \
         } \
-        else if(args == 0) { \
+        else if(args == 1) { \
             lua_pushboolean(state, self->field); \
         } \
         else { \
